95c9dfd6cbc337b6671b2caea1ad473d29479fbe
tamires01-alt <rtamires117@gmail.com>
2024-08-09 19:06:55 -0300

    fix: responsive image Background

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,6 @@
           <DialogBoxRight
               message="jkdkkkkkkkkkkkkkkkkkkkkkkhdjsdadd ddasdkkkkkkkkkkkkk"
           />
-       
         </div>
 
         <div style="margin-top: 20px;">
@@ -22,10 +21,14 @@
             <dialogBoxAudio   :audioSrc="audioFile02" :imageProfile="imageProfile"/>
           </div>
         </div>
+
+        <div>
+          <dialogBoxImage  :imageContent="imageContent"/>
+        </div>
       
-       <div style="margin-left: 900px;;">
+       <!-- <div style="margin-left: 900px;;">
         <MapComponent/>
-       </div>
+       </div> -->
        
     </div>
   </div>
@@ -33,34 +36,45 @@
 
 <script>
 
-import MapComponent from './components/map.vue';
+// import MapComponent from './components/map.vue';
 import DialogBoxRight from './components/dialogBoxRight.vue';
 import DialogBoxRightLeft from './components/dialogBoxLeft.vue';
 import dialogBoxAudio from './components/dialogBoxAudio.vue';
+import dialogBoxImage from './components/dialogBoxImage.vue';
 
 export default {
   name: 'App',
   components: {
-    MapComponent,
+    // MapComponent,
     DialogBoxRightLeft,
     DialogBoxRight,
-    dialogBoxAudio
+    dialogBoxAudio,
+    dialogBoxImage 
     
   },
   data() {
     return {
       audioFile01: require('@/assets/audio/benca.mp3'),
       audioFile02: require('@/assets/audio/audi01.mp3'),
-      imageProfile: require('@/assets/image/perfilPersonagem02.jpeg')
+      imageProfile: require('@/assets/image/perfilPersonagem02.jpeg'),
+      imageContent: require('@/assets/image/perfilPersonagem02.jpeg')
     };
   }
 };
 </script>
 
 <style scoped>
+
+
+
 .app-background {
-  background-image: url(assets/image/backgroundDark.jpeg);
-  min-height: 100vh;
-  margin: 0;
+  background-image: url('assets/image/backgroundDark.jpeg');
+ 
+  min-height: 100px; 
 }
+
+
+
+
+
 </style>
---
+++ src/components/dialogBoxImage.vue
@@ -0,0 +1,52 @@
+<template>
+    <div style="display: flex;">
+        <div class="pointerUserLeft"></div>
+        <div id="boxStyleComponentLeft">
+            <div>
+                <img class="imageContent" :src="this.imageContent"/>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+
+export default {
+  props: {
+    imageContent: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+    };
+  },
+  methods: {
+   
+  },
+};
+</script>
+<style>
+#boxStyleComponentLeft{
+    width: 50%;
+    height: 256px;
+    padding: 30px 15px;
+    border-radius: 0px 20px 20px 20px ;
+    background-color: #f4f4f4;
+    text-align: center;
+    color: #0b141a;
+}
+
+.pointerUserLeft{
+    background-color: #f4f4f4;
+    border-radius: 0px 0px 0px 400px ;
+    width: 12px;
+    height: 14px;
+}
+
+.imageContent{
+    background-color: #bd1313;
+    width: 252px;
+    height: 204px; 
+}
+</style>
