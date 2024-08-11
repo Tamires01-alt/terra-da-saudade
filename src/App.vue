fb24d5d633dd4b2adf9fe1de6d0832b1a3bc7087
tamires01-alt <rtamires117@gmail.com>
2024-08-11 20:14:20 -0300

    temp component

--- src/App.vue
+++ src/App.vue
@@ -16,78 +16,94 @@
            v-if="showbox4"
             message="Ao 17 anos o menino de quijingue foi explorando a quarta maior cidade do mundo, andando entre o centro e a periferia, e fazendo naquele momento sua morada o campo limpo, extremo sul, São paulo  "
           />
-          <dialogBoxImage :imageContent="pracaRepublica" />
-          <dialogBoxImage :imageContent="CampoLimpo" />
+          <dialogBoxImage 
+            v-if="showbox5" 
+            :imageContent="pracaRepublica" />
+          <dialogBoxImage 
+             v-if="showbox6"  :imageContent="CampoLimpo" />
 
           <!-- Exibe o componente DialogBoxRightLeft após 2 segundos -->
           <DialogBoxRightLeft
+            v-if="showbox7"
             message="Depois de um tempo conseguiu conquistar o sonho de consumo de muitos na época dos anos 80, um fusquinha, e um black power"
           />
-          <dialogBoxImage :imageContent="personagem08" />
+          <dialogBoxImage v-if="showbox8" :imageContent="personagem08" />
           <DialogBoxRightLeft
+           v-if="showbox9"
             message="Mas com o tempo a responsa bateu na sua porta, e as prioridade de da  suporte para sua família foi maior e vendeu seu fusquinha na época do cruzeiro para da entrada em um terreno no jardim ângela , periferia da zona sul de São Paulo"
           />
-          <dialogBoxImage :imageContent="escrituraTerreno" />
-          <dialogBoxImage :imageContent="materia" />
+          <dialogBoxImage v-if="showbox10" :imageContent="escrituraTerreno" />
+          <dialogBoxImage v-if="showbox11" :imageContent="materia" />
           <DialogBoxRightLeft
+            v-if="showbox12"
             message="Lembro que foi construindo a casa , para sairmos do aluguel , e depois quando eu tinha 5 anos , todos nós mudamos para a casa, que lá construiremos nossas histórias"
           />
-          <dialogBoxImage :imageContent="personagem96" />
+          <dialogBoxImage v-if="showbox13" :imageContent="personagem96" />
 
           <DialogBoxRightLeft
+          v-if="showbox14"
             message="E depois de alguns anos , tijolo  por tijolo , com muita trabalho e esforço de toda família, fizemos daquele simples terreno , um lar , que aos domingos fazemos um churrasco na laje"
           />
 
-          <dialogBoxImage :imageContent="personagem988" />
+          <dialogBoxImage  v-if="showbox15" :imageContent="personagem988" />
           <DialogBoxRightLeft
+           v-if="showbox16"
             message="Dia 16 de março de 2016 , as 10 e 26 da manhã, você se torna vô do Henrique Rodrigues, e seu olhar se ilumina"
           />
 
-          <dialogBoxImage :imageContent="personagem995" />
-          <dialogBoxImage :imageContent="imageContent" />
-          <dialogBoxImage :imageContent="personagem90" />
+          <dialogBoxImage v-if="showbox17"  :imageContent="personagem995" />
+          <dialogBoxImage v-if="showbox18" :imageContent="imageContent" />
+          <dialogBoxImage v-if="showbox19" :imageContent="personagem90" />
 
           <DialogBoxRightLeft
+           v-if="showbox20"
             message="Mas eu ainda tô aqui perguntando Pai, quantas saudades você carrega ?"
           />
           <DialogBoxRightLeft
+          v-if="showbox21"
             message=" A primeira vez que te vi chorar foi no velório do vô Henrique , seu Pai , o ano era início de 2021. No começo de 2020 você estava cruzando a cidade para trazer ele para São Paulo , seria uma breve viagem , mas com a pandemia e tudo parado, vô Henrique ficou em São Paulo, esperando as coisas se normalizarem"
           />
           <DialogBoxRightLeft
+          v-if="showbox22"
             message=" Aos 97 anos Vo Henrique Partiu, seu coração  parou de bater no final do dia , e assim sua alma voou como uma pássaro, terminando sua história aqui na terra da saudade, onde seus filhos vieram para ganhar a vida "
           />
-          <dialogBoxImage :imageContent="personagem09" />
-          <DialogBoxRightLeft message="Quantas saudades você carrega Pai? " />
-          <dialogBoxAudio :audioSrc="audio30" :imageProfile="imageProfile" />
+          <dialogBoxImage v-if="showbox23" :imageContent="personagem09" />
+          <DialogBoxRightLeft v-if="showbox24" message="Quantas saudades você carrega Pai? " />
+          <dialogBoxAudio v-if="showbox25"  :audioSrc="audio30" :imageProfile="imageProfile" />
           <DialogBoxRightLeft
+            v-if="showbox26"
             message="Em maio de 2024, você resolve voltar para sua terrinha, e matar um pouco da saudade, vendo os seus, comendo sua comida, andando pela cidade, e balançando na sua rede no quijingue"
           />
-          <dialogBoxAudio :audioSrc="audio31" :imageProfile="imageProfile" />
-          <MapComponent />
+          <dialogBoxAudio v-if="showbox27" :audioSrc="audio31" :imageProfile="imageProfile" />
+          <MapComponent v-if="showbox28" />
 
           <DialogBoxRightLeft
+           v-if="showbox29"
             message="Aos 72 anos, você cruza estados do Brasil Pai , sem olhar para trás, mas deixando saudade a cada áudio mandado"
           />
 
-          <dialogBoxAudio :audioSrc="audio32" :imageProfile="imageProfile" />
+          <dialogBoxAudio  v-if="showbox30" :audioSrc="audio32" :imageProfile="imageProfile" />
 
           <DialogBoxRightLeft
+           v-if="showbox31"
             message="Imagino que muitas vezes está de imigrante nessa terra foi difícil, ter construído essa cidade e recebido tão pouco de volta, foi muito difícil, mas valeu por tudo, me ensina e me ensinou muito "
           />
 
-          <dialogBoxAudio :audioSrc="audio33" :imageProfile="imageProfile" />
+          <dialogBoxAudio  v-if="showbox32" :audioSrc="audio33" :imageProfile="imageProfile" />
 
           <DialogBoxRightLeft
-            message="Meu anjo da guarda que eu posso tocar "
+            v-if="showbox33"
+            message="Meu anjo da guarda que  posso tocar "
           />
 
-          <dialogBoxAudio :audioSrc="audio34" :imageProfile="imageProfile" />
+          <dialogBoxAudio v-if="showbox34" :audioSrc="audio34" :imageProfile="imageProfile" />
 
           <DialogBoxRightLeft
-            message="Meu anjo da guarda que eu posso tocar "
+           v-if="showbox35"
+            message="Hoje somos todos a continuação do seu sonho e dos que vieram antes de você  "
           />
-          <dialogBoxImage :imageContent="personagem70" />
-          <dialogBoxImage :imageContent="personagem60" />
+          <dialogBoxImage  v-if="showbox36"  :imageContent="personagem70" />
+          <dialogBoxImage v-if="showbox37"  :imageContent="personagem60" />
         </div>
       </div>
     </div>
@@ -135,28 +151,153 @@ export default {
       showbox2: false,
       showbox3: false,
       showbox4: false,
-      
-      showDialogBoxAudio: false,
-      showDialogBoxImage: false,
-      showMapComponent: false,
+      showbox5: false,
+      showbox6: false,
+      showbox7: false,
+      showbox8: false,
+      showbox9: false,
+      showbox10: false,
+      showbox11: false,
+      showbox12: false,
+      showbox13: false,
+      showbox14: false,
+      showbox15: false,
+      showbox16: false,
+      showbox17: false,
+      showbox18: false,
+      showbox19: false,
+      showbox20: false,
+      showbox21: false,
+      showbox22: false,
+      showbox23: false,
+      showbox24: false,
+      showbox25: false,
+      showbox26: false,
+      showbox27: false,
+      showbox28: false,
+      showbox29: false,
+      showbox30: false,
+      showbox31: false,
+      showbox32: false,
+      showbox33: false,
+      showbox34: false,
+      showbox35: false,
+      showbox36: false,
+      showbox37: false,
     };
   },
   mounted() {
     setTimeout(() => {
       this.showbox2 = true;
-    }, 17000); // 2 segundos
+    }, 17000); 
 
     setTimeout(() => {
       this.showbox3= true;
-    }, 51000); // 3 segundos
+    }, 51000); 
 
     setTimeout(() => {
       this.showbox4 = true;
-    }, 53000); // 4 segundos
-
+    }, 53000);
+    
     setTimeout(() => {
-      this.showMapComponent = true;
-    }, 50000); // 5 segundos
+      this.showbox5 = true;
+    }, 60000); 
+    setTimeout(() => {
+      this.showbox6 = true;
+    }, 61000);
+    setTimeout(() => {
+      this.showbox7 = true;
+    }, 68000);
+    setTimeout(() => {
+      this.showbox8 = true;
+    }, 70000);
+    setTimeout(() => {
+      this.showbox9 = true;
+    }, 80000);
+    setTimeout(() => {
+      this.showbox10 = true;
+    }, 82000);
+    setTimeout(() => {
+      this.showbox11 = true;
+    }, 83000);
+    setTimeout(() => {
+      this.showbox12 = true;
+    }, 97000);
+    setTimeout(() => {
+      this.showbox13 = true;
+    }, 99000);
+    setTimeout(() => {
+      this.showbox14 = true;
+    }, 109000);
+    setTimeout(() => {
+      this.showbox15 = true;
+    }, 109000);
+    setTimeout(() => {
+      this.showbox16 = true;
+    }, 122000);
+    setTimeout(() => {
+      this.showbox17 = true;
+    }, 124000);
+    setTimeout(() => {
+      this.showbox18 = true;
+    }, 125000);
+    setTimeout(() => {
+      this.showbox19 = true;
+    }, 126000);
+    setTimeout(() => {
+      this.showbox20 = true;
+    }, 128000);
+    setTimeout(() => {
+      this.showbox21 = true;
+    }, 138000);
+    setTimeout(() => {
+      this.showbox22 = true;
+    }, 160000);
+    setTimeout(() => {
+      this.showbox23 = true;
+    }, 168000);
+    setTimeout(() => {
+      this.showbox24 = true;
+    }, 182000);
+    setTimeout(() => {
+      this.showbox25 = true;
+    }, 186000);
+    setTimeout(() => {
+      this.showbox26 = true;
+    }, 206000);
+    setTimeout(() => {
+      this.showbox27 = true;
+    }, 216000);
+    setTimeout(() => {
+      this.showbox28 = true;
+    }, 216000);
+    setTimeout(() => {
+      this.showbox29 = true;
+    }, 220000);
+    setTimeout(() => {
+      this.showbox30 = true;
+    }, 226000);
+    setTimeout(() => {
+      this.showbox31 = true;
+    }, 236000);
+    setTimeout(() => {
+      this.showbox32 = true;
+    }, 239000);
+    setTimeout(() => {
+      this.showbox33 = true;
+    }, 266000);
+    setTimeout(() => {
+      this.showbox34 = true;
+    }, 270000);
+    setTimeout(() => {
+      this.showbox35 = true;
+    }, 273000);
+    setTimeout(() => {
+      this.showbox36 = true;
+    }, 278000);
+    setTimeout(() => {
+      this.showbox37 = true;
+    }, 279000);
   },
 };
 </script>
